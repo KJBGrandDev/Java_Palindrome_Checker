4e7ec86ac41d7d225d93d6df7399489aedc140a7
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-05-08 00:54:26 +0800

    Docs: Second Iteration

--- Palindrome_Checker_OOP/filePalindromeChecker.java
+++ Palindrome_Checker_OOP/filePalindromeChecker.java
@@ -1,32 +1,48 @@
 package Palindrome_Checker_OOP;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class filePalindromeChecker {
-    private String fileName;
+    private final String fileName;
 
     public filePalindromeChecker(String fileName){
         this.fileName = fileName;
     }
 
     public void palindromeChecker(){
-        File file = new File(fileName);
-        try(Scanner fileReader = new Scanner(file)){
+        try{
+            File file = new File(fileName);
+            Scanner fileReader = new Scanner(file);
+
             if(!fileReader.hasNextLine()){
+                System.out.println("-------------------------------");
                 System.out.println("This file is empty");
+                System.out.println("File checker terminates..");
+                System.out.println("-------------------------------\n");
                 return;
             }
+
+            System.out.println("Checking file contents: \n");
+
             while (fileReader.hasNextLine()){
                 String nextLine = fileReader.nextLine();
                 palindromeChecker checker = new palindromeChecker(nextLine);
                 System.out.println(nextLine + " = " + checker);
             }
-            System.out.println("\nThe program has fully scanned the file");
+
+            System.out.println("\n-------------------------------");
+            System.out.println("The program has fully scanned the file");
             System.out.println("No more lines had been detected");
-            System.out.println("\nProgram will now terminate..");
-            System.out.println("Goodbye!");
-        } catch(Exception e){
+            System.out.println("\nFile checker terminates..");
+            System.out.println("-------------------------------\n");
+
+        }catch(FileNotFoundException e){
+            System.out.println("-------------------------------");
+            System.out.println("File not found! Please try again");
+            System.out.println("-------------------------------\n");
+        }catch (Exception e){
             System.out.println("Error: " + e);
         }
     }
--- Palindrome_Checker_OOP/program.java
+++ Palindrome_Checker_OOP/program.java
@@ -6,38 +6,55 @@ public class program {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Choose a category: \n");
-        System.out.println("1. Check a word/phrase");
-        System.out.println("2. Check an entire file\n");
-        System.out.println("Your choice: ");
-        int input = Integer.parseInt(scanner.nextLine());
+        boolean palindromeSystem = true;
+        while(palindromeSystem){
+            System.out.println("Choose a category: \n");
+            System.out.println("1. Check a word/phrase");
+            System.out.println("2. Check an entire file");
+            System.out.println("3. Exit\n");
+            System.out.println("Your choice: ");
+            String input = scanner.nextLine();
 
-        switch (input){
-            case 1:{
-                while (true){
-                    System.out.println("\nInput a word or phrase, giving a blank space will end the program: ");
-                    String wordInput = scanner.nextLine();
+            try{
+                int inputTrial = Integer.parseInt(input);
+                if(inputTrial < 1 || inputTrial > 3){
+                    System.out.println("Invalid Choice. Please try again\n");
+                }
+                switch (inputTrial){
+                    case 1:{
+                        while (true){
+                            System.out.println("\nInput a word or phrase, giving a blank space will end the program: ");
+                            String wordInput = scanner.nextLine();
 
-                    if(wordInput.isEmpty()){
-                        System.out.println("You didn't give a word/phrase\n");
-                        System.out.println("Program will now terminate..");
-                        System.out.println("Goodbye!");
+                            if(wordInput.isEmpty()){
+                                System.out.println("You didn't give a word/phrase");
+                                System.out.println("Word checker terminates..\n");
+                                break;
+                            }
+
+                            palindromeChecker checker = new palindromeChecker(wordInput);
+                            System.out.println("Answer: " + checker);
+                        }
                         break;
                     }
+                    case 2:{
+                        System.out.println("\nInput a file name: ");
+                        String fileNameInput = scanner.nextLine();
+                        System.out.println();
 
-                    palindromeChecker checker = new palindromeChecker(wordInput);
-                    System.out.println("Answer: " + checker);
+                        filePalindromeChecker checker = new filePalindromeChecker(fileNameInput);
+                        checker.palindromeChecker();
+                        break;
+                    }
+                    case 3:{
+                        System.out.println("\nProgram will now terminate..");
+                        System.out.println("Goodbye!");
+                        palindromeSystem = false;
+                        break;
+                    }
                 }
-                break;
-            }
-            case 2:{
-                System.out.println("\nInput a file name: ");
-                String fileNameInput = scanner.nextLine();
-                System.out.println();
-
-                filePalindromeChecker checker = new filePalindromeChecker(fileNameInput);
-                checker.palindromeChecker();
-                break;
+            }catch (NumberFormatException e){
+                System.out.println("\nInvalid Choice. Please try again\n");
             }
         }
     }
